68393146b6735e00741bad3809ac699531ccbfd1
basselalsayed <alsayed.bassel@gmail.com>
2020-06-07 00:41:51 +0100

    signup form working with firebase, using higher order components and automatic routing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
-import logo from './logo.svg';
-import './App.css';
-import Firebase, { FirebaseContext } from './components/firebase';
+// import React from 'react';
+// import { Form, Button } from 'react-bootstrap';
+// import logo from './logo.svg';
+// import './App.css';
+// import Firebase, { FirebaseContext } from './components/firebase';
 
-function App() {
-  const handleSubmit = (e) => {
-    console.log(e);
-  };
+// function App() {
+//   const handleSubmit = (e) => {
+//     console.log(e);
+//   };
 
-  return (
-    <FirebaseContext.Provider value={new Firebase()}>
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
+//   return (
+//     <FirebaseContext.Provider value={new Firebase()}>
+//       <div className='App'>
+//         <header className='App-header'>
+//           <img src={logo} className='App-logo' alt='logo' />
 
-          <Form onSubmit={(e) => handleSubmit(e)}>
-            <Form.Group controlId='formBasicEmail'>
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type='email' placeholder='Enter email' />
-              <Form.Text className='text-muted'>
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+//           <Form onSubmit={(e) => handleSubmit(e)}>
+//             <Form.Group controlId='formBasicEmail'>
+//               <Form.Label>Email address</Form.Label>
+//               <Form.Control type='email' placeholder='Enter email' />
+//               <Form.Text className='text-muted'>
+//                 We'll never share your email with anyone else.
+//               </Form.Text>
+//             </Form.Group>
 
-            <Form.Group controlId='formBasicPassword'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type='password' placeholder='Password' />
-            </Form.Group>
-            <Form.Group controlId='formBasicCheckbox'>
-              <Form.Check type='checkbox' label='Check me out' />
-            </Form.Group>
-            <Button variant='primary' type='submit'>
-              Submit
-            </Button>
-          </Form>
-        </header>
-      </div>
-    </FirebaseContext.Provider>
-  );
-}
+//             <Form.Group controlId='formBasicPassword'>
+//               <Form.Label>Password</Form.Label>
+//               <Form.Control type='password' placeholder='Password' />
+//             </Form.Group>
+//             <Form.Group controlId='formBasicCheckbox'>
+//               <Form.Check type='checkbox' label='Check me out' />
+//             </Form.Group>
+//             <Button variant='primary' type='submit'>
+//               Submit
+//             </Button>
+//           </Form>
+//         </header>
+//       </div>
+//     </FirebaseContext.Provider>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,126 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
 
-const SignUp = () => {
-  return <div></div>;
+const SignUpPage = () => (
+  <div>
+    <h1>SignUp</h1>
+    <SignUpForm />}
+  </div>
+);
+
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
 };
+class SignUpFormBase extends Component {
+  constructor(props) {
+    super(props);
 
-export default SignUp;
+    this.state = { ...INITIAL_STATE };
+  }
+
+  handleSubmit = event => {
+    const { username, email, passwordOne } = this.state;
+
+    this.props.firebase
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(authUser => {
+        this.setState({ ...INITIAL_STATE });
+        this.props.history.push(ROUTES.HOME);
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+
+    event.preventDefault();
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { username, email, passwordOne, passwordTwo, error } = this.state;
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === '' ||
+      email === '' ||
+      username === '';
+
+    return (
+      <Form onSubmit={e => this.handleSubmit(e)}>
+        <Form.Group controlId='formBasicUsername'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            name='username'
+            value={username}
+            onChange={this.onChange}
+            type='text'
+            placeholder='Enter username'
+          />
+          <Form.Text className='text-muted'>
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            name='email'
+            value={email}
+            onChange={this.onChange}
+            type='text'
+            placeholder='Email Address'
+          />
+          <Form.Text className='text-muted'>
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            name='passwordOne'
+            value={passwordOne}
+            onChange={this.onChange}
+            type='password'
+            placeholder='Password'
+          />
+        </Form.Group>
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            name='passwordTwo'
+            value={passwordTwo}
+            onChange={this.onChange}
+            type='password'
+            placeholder='Confirm Password'
+          />
+        </Form.Group>
+        <Button variant='primary' type='submit' disabled={isInvalid}>
+          Sign Up
+        </Button>
+
+        {error && <p>{error.message}</p>}
+      </Form>
+    );
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
+const SignUpForm = withRouter(withFirebase(SignUpFormBase));
+
+export default SignUpPage;
+
+export { SignUpForm, SignUpLink };
