ba391f1e7bc89e92129f56bb8e629ec8c719cffa
basselalsayed <alsayed.bassel@gmail.com>
2020-06-07 01:27:10 +0100

    signout button

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-import * as ROUTES from '../../constants/routes';
+import SignOutButton from '../SignOut'
+
+import * as ROUTES from '../../constants/routes'
 
 const Navigation = () => (
   <div>
@@ -21,8 +23,11 @@ const Navigation = () => (
       <li>
         <Link to={ROUTES.ADMIN}>Admin</Link>
       </li>
+      <li>
+        <SignOutButton />{' '}
+      </li>
     </ul>
   </div>
-);
+)
 
-export default Navigation;
+export default Navigation
