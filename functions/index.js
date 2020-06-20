7302511cb1a02a46f71f78ed82a10eb3f4ede299
basselalsayed <alsayed.bassel@gmail.com>
2020-06-20 14:19:59 +0100

    readme

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,7 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
 
-// firebase.initializeAdminApp({ projectId: "test-8af68" });
+admin.initializeApp(functions.config().firebase);
 
 exports.sendMessage = functions.firestore
   .document(`products/{productId}`)
@@ -27,6 +18,6 @@ exports.sendMessage = functions.firestore
         { message: `Nice ${data.name} - from functions` },
         {
           merge: true,
-        }
+        },
       );
   });
